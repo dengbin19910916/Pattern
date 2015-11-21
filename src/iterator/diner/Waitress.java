4c5093070285815edf643979833a28f19be667a5
柳叶清 <dengbin19910916@163.com>
2015-11-21 08:17:12 +0800

    修改Waitress

--- src/iterator/diner/Waitress.java
+++ src/iterator/diner/Waitress.java
@@ -1,6 +1,7 @@
 package iterator.diner;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -9,37 +10,18 @@ import java.util.List;
  */
 public class Waitress {
     private List<Menu> menus;
-    private Menu pancakeHouseMenu;
-    private Menu dinerMenu;
-    private Menu cafeMenu;
 
     public Waitress(List<Menu> menus) {
         this.menus = menus;
     }
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
-        this.pancakeHouseMenu = pancakeHouseMenu;
-        this.dinerMenu = dinerMenu;
-        this.cafeMenu = cafeMenu;
-        this.menus = new ArrayList<>();
-        menus.add(pancakeHouseMenu);
-        menus.add(dinerMenu);
-        menus.add(cafeMenu);
+    public Waitress(Menu... menus) {
+        this.menus = new ArrayList<>(menus.length);
+        Collections.addAll(this.menus, menus);
     }
 
     public void printMenu() {
         menus.forEach(this::printMenu);
-
-//        Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.iterator();
-//        Iterator<MenuItem> dinerIterator = dinerMenu.iterator();
-        /*System.out.println("MENU\n---\nBREAKFAST");
-//        printMenu(pancakeIterator);
-        printMenu(pancakeHouseMenu);
-        System.out.println("\nLUNCH");
-//        printMenu(dinerIterator);
-        printMenu(dinerMenu);
-        System.out.println("\nDINNER");
-        printMenu(cafeMenu);*/
     }
 
     private void printMenu(Menu menu) {
